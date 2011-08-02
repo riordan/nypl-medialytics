f144daea5cf7a82f1a1a540d0af77249000aa6b9
David Riordan <david.riordan@gmail.com>
2011-08-02 13:42:38 -0400

    Implemented TrackList as the primary interface to Track objects. Created hooks for Preview counts.

--- iTunes.py
+++ iTunes.py
@@ -8,38 +8,50 @@ import dateutil.parser
 import datetime
 
 class Track:
-	def __init__(self,handle, path, count, date):
+	def __init__(self,handle, path):
 		self.handle = handle
 		self.path = path[:path.rfind('>')]
 		#print "The folder is: %s" %self.path
 		self.name = path[path.rfind('>')+2:]
 		#print "The track is: %s" %self.name
-		self.count = {}
-		self.addDate(date,count)
+		self.dlCount = {}
+		self.pvCount = {}
 		
 		'''
 		#Debug code:
 		print "Handle = %i" % self.handle
 		print "Path = %s" % self.path
-		print "DayCount for %s: %i" %(str(self.count.keys()[0].date()), self.count[self.count.keys()[0]])
-		print "TotalCount: %i" %self.downloads()
+		print "DaydlCount for %s: %i" %(str(self.dlCount.keys()[0].date()), self.dlCount[self.dlCount.keys()[0]])
+		print "TotaldlCount: %i" %self.downloads()
 		'''
-	
 		
-	def addDate(self,date,count):
-		self.count[date] = count
-		# print "TotalCount: %i" %self.downloads()
 	
+	#Adds a new count to Downloads
+	def addDlDate(self,date,dlCount):
+		self.dlCount[date] = dlCount
+		# print "TotaldlCount: %i" %self.downloads()
+	
+	#Adds a new count to Previews
+	def addPvDate(self,date,pvCount):
+		self.pvCount[date] = pvCount
+	
+	#Returns cumulative number of downloads for a track
 	def downloads(self):
 		dls = 0
-		for day in self.count.keys():
-			dls += self.count[day]
+		for day in self.dlCount.keys():
+			dls += self.dlCount[day]
 		return dls
+	
+	#Returns cumulative number of previews for a track
+	def previews(self):
+		pvs = 0
+		for day in self.pvCount.keys():
+			pvs += self.pvCount[day]
 	
 	
 	def __str__(self):
-		return self.name		
-	
+		return self.name
+
 
 
 class TrackList:
@@ -49,12 +61,22 @@ class TrackList:
 		allTracks: a dictionary of tracks indexed by handle
 	
 	Public Methods:
-		hasDate(date) takes a python datetime (or string of a date) and returns true if it is represented in the set of tracks 
-		countDate(date) takes a datetime and returns the total count of downloads for that date. returns 0 if none
+		hasDate(date) takes a python datetime (or string of a date) and returns true if it is represented in the set of tracks
+		dlCountDate(date) takes a datetime and returns the total dlCount of downloads for that date. returns 0 if none
 		trackExists(handle) Takes a handle (numeric identifier) and returns true if that track exists
 	'''
 	def __init__(self):
 		self.allTracks = {}
+		self.allDates = []
+	
+	def newTrack(self,handle,path):
+		self.allTracks[handle] = Track(handle, path)
+	
+	def addDlCount(self, handle, date, dlCount):
+		self.allTracks[handle].addDlDate(date,dlCount)
+		if not date in self.allDates:
+			self.allDates.append(date)
+	
 	
 	def trackExists(self,handle):
 		return handle in self.allTracks.keys()
@@ -70,9 +92,9 @@ class TrackList:
 		
 		#Traverses tracks & dates to see if date is in system
 		for tkey in self.allTracks.keys():
-		  for dkey in range(len(self.allTracks[tkey].count.keys())):
-		    if date == self.allTracks[tkey].count.keys()[dkey]: return True
-		  
+		  for dkey in range(len(self.allTracks[tkey].dlCount.keys())):
+		    if date == self.allTracks[tkey].dlCount.keys()[dkey]: return True
+		
 		return False
 
 
@@ -80,15 +102,12 @@ class TrackList:
 # Imports tracks. Takes filename of import file, returns list of rows in sheet
 # Caveat: works only for workbook of 1 sheet with just tracks
 def importReport(dirName, trackList):
-	
 	#Traverses directory looking for iTunes excel files
-	
 	dlist = os.listdir(dirName)
-	for fileName in glob.glob( os.path.join(dirName, '*.xls') ) :	
+	for fileName in glob.glob( os.path.join(dirName, '*.xls') ) :
 		#opens excel workbook
 		wb = xlrd.open_workbook(fileName)
 		print "in workbook: %s" %fileName
-	
 		'''Opens all Track worksheets in a workbook'''
 		trackSheets = []
 		for sheet in wb.sheet_names():
@@ -96,37 +115,36 @@ def importReport(dirName, trackList):
 				trackSheets.append(sheet)
 		
 		for shName in trackSheets:
-		
+			
 			#opens Track Sheet
 			sh = wb.sheet_by_name(shName)
 			print "In Sheet: %s" %sh.name
 			rows = []
 			#Determines date of data based on name of sheet
-			date = dateutil.parser.parse(sh.name[:sh.name.find(' ')]) 
+			date = dateutil.parser.parse(sh.name[:sh.name.find(' ')])
 			#print date
-	
+			
 			#importer
 			'''Sheet Parser: Tracks
 			Track excel worksheets contain the date in their sheet name and a schema of
-			|PATH|COUNT|HANDLE
+			|PATH|dlCount|HANDLE
 			PATH is the path to the asset (including the actual track name, denoted by
 			the final carrot ">".)
-			COUNT is the number of downloads this week.
+			dlCount is the number of downloads this week.
 			HANDLE is the unique ID of each track.'''
 			for rownum in range(1,sh.nrows):
 				cRow = sh.row_values(rownum)
 				path = sh.row_values(rownum)[0]
-				count = sh.row_values(rownum)[1]
+				dlCount = sh.row_values(rownum)[1]
 				handle = sh.row_values(rownum)[2]
-		
-				#Checks if it's an existing track
-				if trackList.trackExists(handle):
-					#Appends data to an existing track
-					trackList.allTracks[handle].addDate(date,count)
-				else: #creates a new Track
-					trackList.allTracks[handle] = Track(handle, path, count, date)
-			
-	
+				
+				#If this track doesn't exist yet, it creates a new instance
+				if not trackList.trackExists(handle):
+					#print "Creating New Track"
+					trackList.newTrack(handle, path)
+				# Updates the count of downloads for that date
+				#print "Adding Download Record"
+				trackList.addDlCount(handle, date, dlCount)
 	return
 
 
